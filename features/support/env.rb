38626e3744bdf467f30dbf4ac8c16818d294f34e
Jesal Mistry <jesalnmistry@gmail.com>
2013-11-22 23:42:49 +0530

    now we shall take screenshots when them tests fail! yeah! also update gitignore to exclude tmp directory

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,8 @@
+require 'capybara'
 require 'capybara/cucumber'
+require 'capybara-screenshot'
+require 'capybara-screenshot/cucumber'
 
 Capybara.default_driver = :selenium
 Capybara.app_host = 'http://www.flipkart.com'
+Capybara.save_and_open_page_path = 'tmp/capybara'
